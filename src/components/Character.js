75a36470f61e6fc92495bfb6d827b6cb981d4e96
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-05-14 08:13:08 -0600

    fetched character data and rendered each character's name on page

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+`;
+
+const StyledChar = styled.section`
+
+`;
+
+
+export default function Character( {character } ) {
+    return(
+        <StyledContainer>
+            <StyledChar>{character.name}</StyledChar>
+        </StyledContainer>
+    )
+}
